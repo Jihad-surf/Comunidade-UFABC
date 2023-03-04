50cf8b45240fae2939f4758efdf283cd9669eb4a
jihad <jihad.a@aluno.ufabc.edu.br>
2023-03-03 23:57:37 -0300

    teste9

--- configs/settings.py
+++ configs/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'calendarioapp',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',  
+    'whitenoise.runserver_nostatic',    
 ]
 
 MIDDLEWARE = [
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'configs.urls'
@@ -123,22 +124,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # coleta os arquivos estaticos que estao nesse diretorio
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'configs/static')
 ]
+
+# ao rodar o python manage.py collectstatic ele ira salvar os arquivos aqui:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-]
-# ao rodar o python manage.py collectstatic ele ira salvar os arquivos aqui:
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
